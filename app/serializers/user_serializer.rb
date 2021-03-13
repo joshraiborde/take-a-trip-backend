72ae0dee328340570aed278dc6fdbe60139faf04
joshsrai <joshsrai@gmail.com>
2021-03-13 17:04:30 -0500

    added and updated hometown, location, trip, user serializers and sessions and trips controller

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,14 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :name
-  belongs_to :hometown
+  has_many :trips, serializer: TripSerializer
+  
+  attribute :hometown do |user|
+    {
+      city: user.hometown.city,
+      state: user.hometown.state,
+      country: user.hometown.country
+    }
+  end
+
 end
