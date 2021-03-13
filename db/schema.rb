5d8dc48e87ee8cc7c0491bb7c529b9597edc6e9b
joshsrai <joshsrai@gmail.com>
2021-03-13 17:05:43 -0500

    added a name column to the trip table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_201054) do
+ActiveRecord::Schema.define(version: 2021_03_13_204459) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_201054) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
